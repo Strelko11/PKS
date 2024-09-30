821d7ae0f719dc98009611b4aef79970567577d1
Strelko11 <adam.strelec.11@gmail.com>
2024-09-30 19:37:13 +0200

    test commit

--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -1,6 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-using System.Threading.Channels;
-
 Console.WriteLine("Hello, World!");
-Console.WriteLine(" How are you");
+Console.WriteLine("How are you doing something?");
