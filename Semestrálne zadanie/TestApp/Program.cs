b42a39f7a83e3a46d6fa7282f5bc79436f29038f
Strelko11 <adam.strelec.11@gmail.com>
2024-09-30 19:54:27 +0200

    test

---
+++ Semestrálne zadanie/TestApp/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("How are you doing something?");
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine(i);
+}
