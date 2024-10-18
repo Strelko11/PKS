08e09cc158465228e08578bee0b821de4adb97ac
Strelko11 <adam.strelec.11@gmail.com>
2024-10-18 18:24:50 +0200

    tset

---
+++ SemestralneZadanie/Computer2/Server.cs
@@ -0,0 +1,30 @@
+
+
+namespace Computer2;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+public class UDP_server
+{
+    private bool running = true;
+
+    public void Start(string udpIP, int udpPort)
+    {
+        using (Socket sock = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp))
+        {
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse(udpIP), udpPort);
+            sock.Bind(endPoint);
+            Console.WriteLine("Listening for connections on " + udpIP + ":" + udpPort);
+                
+            byte[] buffer = new byte[1024];
+
+            while (running)
+            {
+                EndPoint senderEndPoint = new IPEndPoint(IPAddress.Any, 0);
+                int bytesReceived = sock.ReceiveFrom(buffer, ref senderEndPoint);
+                string receivedMessage = Encoding.ASCII.GetString(buffer, 0, bytesReceived);
+                Console.WriteLine("Received message " + receivedMessage);
+            }
+        }
+    }
+}
