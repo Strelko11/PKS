555792e74b33dfb9e23b1d63b3fe35a8ca06f329
Strelko11 <adam.strelec.11@gmail.com>
2024-11-23 21:00:39 +0100

    rm files

--- SemestralneZadanie/Computer1/obj/Debug/net8.0/Computer1.AssemblyInfo.cs
+++ SemestralneZadanie/Computer1/obj/Debug/net8.0/Computer1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Computer1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f36a52b7283fc9b0306ee23cc8d977ab076fa8e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c36ba544f5cc9deaebf78dc2e0675bb92d01714a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Computer1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Computer1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
